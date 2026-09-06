a4df4db4beee4a5507195713180a855c3bce9d86
NiTiSon <NiTiSon@users.noreply.github.com>
2026-09-06 23:05:03 +0300

    temp generation

---
+++ mod/src/nitis/gravillaso/content/GrLoadouts.java
@@ -0,0 +1,11 @@
+package nitis.gravillaso.content;
+
+import arc.struct.*;
+import mindustry.game.*;
+
+public class GrLoadouts{
+    public static Schematic basicBase /* yeah */ = new Schematic(
+        Seq.with(new Schematic.Stile(GrBlocks.coreBase, 0, 0, null, (byte)0)),
+        new StringMap(), 4, 4
+    );
+}
