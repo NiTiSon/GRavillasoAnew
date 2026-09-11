d863f423dbd6e348e104aa5a925c8eeac5f18f7c
NiTiSon <NiTiSon@users.noreply.github.com>
2026-09-11 19:16:07 +0300

    implement DrawDirectionalPulse.java

--- mod/src/nitis/gravillaso/world/draw/DrawDirectionalPulse.java
+++ mod/src/nitis/gravillaso/world/draw/DrawDirectionalPulse.java
@@ -23,8 +23,34 @@ public class DrawDirectionalPulse extends DrawBlock{
 
     @Override
     public void draw(Building build){
-        int rot = build.rotation;
-        // TODO: implement
-        // draw line from back to front (+rotation)
+        if(!Lod.l2) return;
+
+        float f = (Time.time / 100f) % 1f;
+        float nearness = 1 - 2 * Math.abs(f - 0.5f);
+
+        float heat = build.warmup();
+        float half = build.block.size * tilesize / 2f;
+
+        float pos = Mathf.lerp(-half, half, f);
+
+        // travel = facing direction, line perpendicular to it
+        var dir = Geometry.d4(build.rotation);
+        float dx = dir.x, dy = dir.y;
+        float px = -dy, py = dx;
+
+        Draw.color(color);
+        Draw.alpha(Lod.alpha2);
+        float thickness;
+        Lines.stroke(thickness = (1.5f * nearness + 0.1f) * heat);
+        thickness /= 2;
+
+        Lines.line(
+        build.x + dx * pos + px * (-half + thickness),
+        build.y + dy * pos + py * (-half + thickness),
+        build.x + dx * pos + px * (half - thickness),
+        build.y + dy * pos + py * (half - thickness)
+        );
+
+        Draw.reset();
     }
 }
